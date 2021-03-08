b6c19049dfc5e3c9cf16db7de1c9bf325341ec7a
Kshitinjay <kshitinjay20@gmail.com>
2021-03-09 02:27:05 +0530

    add delete options added

--- surveytiger/src/components/CreateSurvey.js
+++ surveytiger/src/components/CreateSurvey.js
@@ -9,27 +9,40 @@ const CreateSurvey = () => {
   };
 
   const [quesType, setQuesType] = useState(0);
-  const [quesText, setQuesText] = useState('');
-  const [options, setOptions] = useState(
-    [{ uid: random(), value: "" },
-    { uid: random(), value: "" }]
-  );
+  const [quesText, setQuesText] = useState("");
+  const [options, setOptions] = useState([
+    { uid: random(), value: "" },
+    { uid: random(), value: "" },
+  ]);
 
-  const addOptions = ()=>{
-      alert("option added");
-  }
-  const removeOptions = ()=>{
-    alert("option removed");
-}
+  const addOptions = () => {
+    let newOption = { uid: random(), value: "" };
+    let tempOptions = [...options, newOption];
+    setOptions(tempOptions);
+  };
+
+  const removeOptions = (id) => {
+    let tempOptions = [...options];
+    delete tempOptions[id];
+    setOptions(tempOptions);
+    console.log(tempOptions[id]);
+  };
 
   return (
     <div className="col-md-10 offset-md-1 col-12 container">
       <TypeSelector quesType={quesType} setQuesType={setQuesType} />
       {quesType !== 0 ? (
         <>
-          <Question quesText={quesText} setQuesText={setQuesText}/>
-          {options.map((uid,idx)=>{
-              return <Options key={idx} addOptions={addOptions} removeOptions={removeOptions}/>
+          <Question quesText={quesText} setQuesText={setQuesText} />
+          {options.map((uid, idx) => {
+            return (
+              <Options
+                key={idx}
+                id={idx}
+                addOptions={addOptions}
+                removeOptions={removeOptions}
+              />
+            );
           })}
         </>
       ) : null}
--- surveytiger/src/components/Options.js
+++ surveytiger/src/components/Options.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Options = ({addOptions,removeOptions}) => {
+const Options = ({addOptions,removeOptions,id}) => {
   return (
     <div>
       <div className="col-md-10 offset-md-1 col-12 mt-2">
@@ -8,7 +8,7 @@ const Options = ({addOptions,removeOptions}) => {
           <input type="text" className="form-control" placeholder="Add options"/>
           <div className="input-group-append">
               <button className="btn btn-outline-secondary ms-1 me-1" type="button" onClick={()=> addOptions()}>+</button>
-              <button className="btn btn-outline-secondary" type="button" onClick={()=> removeOptions()}>-</button>
+              <button className="btn btn-outline-secondary" id={id}  type="button" onClick={()=> removeOptions(id)}>-</button>
           </div>
         </div>
       </div>
